70cf4d128e747c81263d6dfa5c60c45840f15a76
Ratul Chhibber <ratulchhibber@gmail.com>
2021-02-23 23:58:09 +0530

    * Porting the mock movie dataset creation inside a Data class - companion object

--- app/src/main/java/com/example/recyclerviewsample/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewsample/MainActivity.kt
@@ -12,28 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var movieList = arrayListOf<Movie>(
-            Movie("Iron Man 11", 7),
-            Movie("Superman", 8),
-            Movie("King Kong", 1),
-            Movie("Kung fu", 6),
-            Movie("The Incredibles", 10),
-            Movie("Terminator", 1),
-            Movie("Iron Man", 7),
-            Movie("Superman", 8),
-            Movie("King Kong", 1),
-            Movie("Kung fu", 6),
-            Movie("The Incredibles", 10),
-            Movie("Terminator", 1),
-            Movie("Iron Man", 7),
-            Movie("Superman", 8),
-            Movie("King Kong", 1),
-            Movie("Kung fu", 6),
-            Movie("The Incredibles 99", 10)
-        )
-
         recyclerView.apply {
-            adapter = RecyclerAdapter(movieList)
+            adapter = RecyclerAdapter(Movie.movies())
             addItemDecoration(
                 DividerItemDecoration(
                 this@MainActivity,
--- app/src/main/java/com/example/recyclerviewsample/Movie.kt
+++ app/src/main/java/com/example/recyclerviewsample/Movie.kt
@@ -1,6 +1,37 @@
 package com.example.recyclerviewsample
 
+
+interface Mock {
+    fun movies(): ArrayList<Movie>
+}
+
 data class Movie(
     val name: String,
     val rating: Int
-)
+) {
+    companion object: Mock {
+        override fun movies(): ArrayList<Movie> {
+            return arrayListOf(
+                    Movie("Iron Man 11", 7),
+                    Movie("Superman", 8),
+                    Movie("King Kong", 1),
+                    Movie("Kung fu", 6),
+                    Movie("The Incredibles", 10),
+                    Movie("Terminator", 1),
+                    Movie("Iron Man", 7),
+                    Movie("Superman", 8),
+                    Movie("King Kong", 1),
+                    Movie("Kung fu", 6),
+                    Movie("The Incredibles", 10),
+                    Movie("Terminator", 1),
+                    Movie("Iron Man", 7),
+                    Movie("Superman", 8),
+                    Movie("King Kong", 1),
+                    Movie("Kung fu", 6),
+                    Movie("The Incredibles 99", 10)
+            )
+        }
+    }
+
+
+}

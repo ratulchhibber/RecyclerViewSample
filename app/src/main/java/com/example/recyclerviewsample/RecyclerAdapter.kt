5edf87d3373c4fc6aff636033e53de03d4d4c87a
Ratul Chhibber <ratulchhibber@gmail.com>
2021-02-24 16:23:48 +0530

    * Adding support for Multiple Layouts in RecyclerView

--- app/src/main/java/com/example/recyclerviewsample/RecyclerAdapter.kt
+++ app/src/main/java/com/example/recyclerviewsample/RecyclerAdapter.kt
@@ -5,26 +5,73 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.recycler_row.view.*
 
 class RecyclerAdapter(private var movieList: ArrayList<Movie>):
-    RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    enum class ViewTypes {
+        Horizontal, Vertical
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val layoutInflator = LayoutInflater.from(parent.context)
-        val view = layoutInflator.inflate(R.layout.recycler_row, parent, false)
-        return ViewHolder(view)
+
+        if (viewType == ViewTypes.Horizontal.ordinal) {
+            val view = layoutInflator.inflate(R.layout.recycler_row, parent, false)
+            return ViewHolderHorizontal(view)
+        }
+
+        //Vertical Layout
+        val view = layoutInflator.inflate(R.layout.recycler_row_vertical_elements, parent, false)
+        return ViewHolderVertical(view)
     }
 
     override fun getItemCount(): Int = movieList.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val movie = movieList[position]
-        holder.bind(movie)
+    override fun getItemViewType(position: Int): Int {
+        return when(position % 2) {
+            0 -> ViewTypes.Horizontal.ordinal
+            1 -> ViewTypes.Vertical.ordinal
+            else -> { throw IllegalArgumentException("Invalid position") }
+        }
     }
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val movie = movieList[position]
+
+        if (holder is ViewHolderHorizontal) {
+            holder.bind(movie)
+        } else if (holder is ViewHolderVertical) {
+            holder.bind(movie)
+        }
+    }
+
+    class ViewHolderHorizontal(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        init {
+            itemView.setOnClickListener {
+                //   val snack = "Item position clicked: $adapterPosition"
+                Snackbar
+                        .make(itemView,
+                                "Sample snack bar message",
+                                Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Click Me") {
+                            Snackbar
+                                    .make(itemView,
+                                            "You clicked the button on snack bar",
+                                            Snackbar.LENGTH_SHORT).show()
+                        }.show()
+            }
+        }
+
+        fun bind(movie: Movie) {
+            itemView.movieNameTextView.text = movie.name
+            itemView.ratingTextView.text = movie.rating.toString()
+        }
+    }
+
+    class ViewHolderVertical(itemView: View): RecyclerView.ViewHolder(itemView) {
 
         init {
             itemView.setOnClickListener {
